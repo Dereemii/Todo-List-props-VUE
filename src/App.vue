f5e912c02cfe2ab59e10f0c9be277a391165822d
Dereemii <leshermira@gmail.com>
2020-09-20 21:47:40 -0300

    todo props

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,97 @@
 <template>
-  <div id="app">
-    {{ message }}
-  </div>
-</template>
+  <div id="app" class="container ">
+    <div id="todo">
+        <h1 id="titulo" >Todo List Vue </h1>
 
+    <div id="tarea">
+        <h2 class="mt-2">{{message}}</h2>
+        <div class="agregar row">
+          <input type="text" v-model="todo" placeholder="Ingresa una nueva tarea" class="col" @keyup.enter="add_todo">
+        <button v-on:click=add_todo class=" btn button-coral "> Agregar </button>
+        </div>
+
+        <ul class="px-0">
+          
+            <li v-for="(todo, index) in todos"
+                :todo="todo" 
+                :index="index"
+                :key="todo.id" 
+                >
+                &hearts;
+                    {{todo}}
+                    <delete-component :id="index" @remove="remove_todo"></delete-component>
+ 
+            </li>
+        </ul>
+    </div>
+
+    </div>
+  
+  </div>
+
+
+</template>
 <script>
+import remove from "./components/deleteComponent.vue"
   export default {
-    data() {
+      data() {
       return {
-        message: 'Hola en vue',
-      };
+        message: "Crea una nueva tarea",
+        todos:[],
+        todo:"",
+    };
+    },
+    components:{
+      'delete-component':remove,
+      },
+    methods:{
+        add_todo: function (todo, index) { 
+            if(this.todo != ""){
+            this.todos.push(this.todo)
+            this.todo="";
+            }
+        },
+        remove_todo(todo){
+           const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        }
     }
   }
 </script>
 
-<style scoped>
-  #app {
-    font-size: 25px;
-    font-family: "Calibri", sans-serif;
-    color: darkblue;
-    background: lightblue;
-  }
+<style >
+li{
+    text-decoration: none;
+    list-style: none;
+}
+h1{
+    font-size: 30px;
+}
+h2{
+    font-size: 20px;
+}
+
+#todo{
+    border: 2px solid coral;
+    padding: 1em;
+    margin: auto
+}
+
+.button-coral{
+    background-color: coral;
+    color: white;
+}
+
+i{
+    float: right;
+}
+
+li{
+    border-bottom: 1px solid coral;
+    margin-top: 2px;
+}
+
+.agregar{
+    margin: auto
+}
 </style>
---
+++ src/components/deleteComponent.vue
@@ -0,0 +1,20 @@
+<template>
+        <i id="app" @click="remove(id)" class="fas fa-trash-alt"></i>
+</template>
+
+<script>
+  export default {
+    name: "removeComponent",
+    props:{
+      id:{
+          type:Number,
+          required:true,
+      },
+    },
+    methods:{
+        remove(id){
+        this.$emit("remove",{id:id})
+        },
+    }
+  }
+</script>
